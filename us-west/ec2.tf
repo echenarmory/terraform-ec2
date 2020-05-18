dd6e648fa631d608039f2152c9490d2960baed11
echenarmory <58048295+echenarmory@users.noreply.github.com>
2020-05-18 12:28:54 -0700

    Update ec2.tf

--- us-west/ec2.tf
+++ us-west/ec2.tf
@@ -22,7 +22,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "0813245c0939ab3ca"
+  ami           = "ami-0813245c0939ab3ca"
   instance_type = "t2.micro"
 
   tags = {
