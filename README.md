5ff6f87242fa0e03e9949fa477882f560beabcef
Ed Chen <ed.chen@armory.io>
2020-05-18 09:54:11 -0700

    added tf script for ec2

---
+++ README.md
@@ -0,0 +1,1 @@
+terraform script to create ec2 image
---
+++ ec2.tf
@@ -0,0 +1,31 @@
+# from https://www.terraform.io/docs/providers/aws/r/instance.html
+# Create a new instance of an Ubuntu 16.04 on an
+# t2.micro node with an AWS Tag naming it "armory-pov"
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-????????"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "0a4fbfab82b4aee91"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "armory-pov"
+  }
+}
