e8f3c32ea01d9a6bdc71ac846b502f827359d629
Ed Chen <ed.chen@armory.io>
2020-05-18 18:42:16 -0700

    copying justin's main.tf

--- us-west/ec2.tf
+++ us-west/ec2.tf
@@ -7,13 +7,17 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
-variable "environment_time" {
-  default = "{{timestamp}}"
+variable "environment_name" {
+  default = "hello"
 }
 
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "echen-terraform-tempbucket"
-  acl = "public-read"
+resource "aws_s3_bucket" "b" {
+  bucket = "terraformer-demo-${var.environment_name}"
+  acl    = "public-read"
+
+  tags = {
+    Name = "Bucket for ${var.environment_name}"
+  }
 }
 
 data "aws_ami" "ubuntu" {
